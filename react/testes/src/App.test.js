58e6ec0fe859842081ac62df620a17e6b88d6ae5
artphil <artphil.dev@gmail.com>
2021-12-21 10:57:23 -0300

    testes unitários

--- react/testes/src/App.test.js
+++ react/testes/src/App.test.js
@@ -2,25 +2,36 @@ import React from 'react';
 
 import { render, screen } from '@testing-library/react';
 
-import App from './App';
+import App, { calcularNovoSaldo } from './App';
 
 describe('Componente principal', () => {
   describe('Quando eu abro o app do banco', () => {
 
-    test('O nome é exibido', () => {
+    test('o nome é exibido', () => {
       render(<App />);
       expect(screen.getByText('ByteBank')).toBeInTheDocument();
-    })
+    });
 
-    test('O saldo é exibido', () => {
+    test('o saldo é exibido', () => {
       render(<App />);
       expect(screen.getByText('Saldo:')).toBeInTheDocument();
-    })
+    });
 
-    test('O botão de transção é exibido', () => {
+    test('o botão de transção é exibido', () => {
       render(<App />);
       expect(screen.getByText('Realizar operação')).toBeInTheDocument();
-    })
+    });
     
-  })
+  });
+
+  describe('Quando eu realizo uma transação', () => {
+    test('que é saque, o valor vai diminuir', () => {
+      const valores = {
+        transacao: 'saque',
+        valor: 50
+      };
+      const novoSaldo = calcularNovoSaldo(valores, 150);
+      expect(novoSaldo).toBe(100);
+    });
+  });
 })
