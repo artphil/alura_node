45a95741d41d9a45ee11a6a214c3d8b878042d7b
artphil <artphil.dev@gmail.com>
2021-12-28 11:03:21 -0300

    Fim do curso de styled-components

--- react/styled-components/src/App.js
+++ react/styled-components/src/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import GlobalStyle from "./Components/GlobalStyled";
+import SwitcherTema from "./Components/SwitcherTema";
+import { BotaoTema } from "./Components/UI";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const toggleTema = () => {
+    setTema(!tema);
+  };
+
   return (
-    <>
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
+      <GlobalStyle />
+      <BotaoTema onClick={toggleTema}>
+        <SwitcherTema tema={tema} />
+      </BotaoTema>
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
