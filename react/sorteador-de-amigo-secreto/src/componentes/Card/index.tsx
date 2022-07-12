e3c6e7b19d07f6ed09a03ac963ad7ef17ebc7045
artphil <artphil.dev@gmail.com>
2022-07-12 14:13:47 -0300

    criando compoente Card

---
+++ react/sorteador-de-amigo-secreto/src/componentes/Card/index.tsx
@@ -0,0 +1,11 @@
+import './estilos.css'
+
+const Card: React.FC = ({ children }) => {
+    return (
+        <div className="card">
+            {children}
+        </div>
+    )
+}
+
+export default Card
