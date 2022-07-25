02fe12ebf8491e0366c5d35aac0b4f5f9c3fce07
artphil <artphil.dev@gmail.com>
2022-07-25 09:02:26 -0300

    editando layout

--- react/sorteador-de-amigo-secreto/src/paginas/Sorteio.tsx
+++ react/sorteador-de-amigo-secreto/src/paginas/Sorteio.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+
 import { useListaParticipantes } from "../state/hook/useListaParticipantes";
 import { useResultadoSorteio } from "../state/hook/useResultadoSorteio";
+
+import Card from "../componentes/Card";
+
+import './Sorteio.css'
 
 function Sorteio() {
 
@@ -12,31 +17,47 @@ function Sorteio() {
 
   const sortear = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault()
-    if(resultado.has(participanteDaVez)){
+    if (resultado.has(participanteDaVez)) {
       setAmigoSecreto(resultado.get(participanteDaVez)!)
     }
   }
 
-  return ( 
-    <section>
+  return (<Card>
+
+    <section className="sorteio">
+      <h2>Quem vai tirar o papelzinho?</h2>
       <form onSubmit={sortear}>
-        <select 
-        required
-        name="participanteDaVez" 
-        id="participanteDaVez"
-        value={participanteDaVez}
-        placeholder='Selecione o seu nome'
-        onChange={evento => setParticipanteDaVez(evento.target.value)}
+        <select
+          required
+          name="participanteDaVez"
+          id="participanteDaVez"
+          value={participanteDaVez}
+          placeholder='Selecione o seu nome'
+          onChange={evento => setParticipanteDaVez(evento.target.value)}
         >
-          {participantes.map(participante => 
-          <option key={participante}>{participante}</option>)
+          {participantes.map(participante =>
+            <option key={participante}>{participante}</option>)
           }
         </select>
-      <button>Sortear</button>
-      {amigoSecreto && <p role='alert'>{amigoSecreto}</p>}
+        <p>Clique em em sortear para ver quem é seu amigo secreto!</p>
+        <button
+          className="botao-sortear"
+          disabled={!participanteDaVez}
+        >Sortear</button>
       </form>
+      {amigoSecreto &&
+        <p className="resultado" role='alert'>{amigoSecreto}</p>
+      }
+      <footer className="sorteio">
+        <img
+          src="/imagens/aviao.png"
+          className="aviao"
+          alt="Um desenho de um avião de papel"
+        />
+      </footer>
     </section>
-   );
+  </Card>
+  );
 }
 
 export default Sorteio;
