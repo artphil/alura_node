c62e281b83e0b3a66ab5a9e3dca05747e01422d5
Arthur Phillip Silva <artphil.dev@gmail.com>
2024-01-11 11:31:16 -0300

    Criando pasta raiz para absolute import

--- react/aluroni/src/pages/Cardapio/index.tsx
+++ react/aluroni/src/pages/Cardapio/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import styles from "./Cardapio.module.css";
-import {ReactComponent as Logo} from "../../assets/logo.svg";
+import {ReactComponent as Logo} from "assets/logo.svg";
 
 
 function Cardapio() {
