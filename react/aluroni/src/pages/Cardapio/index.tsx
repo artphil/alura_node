6f40a468eca26be917681ec095004b77458cb3d5
Arthur Phillip Silva <artphil.dev@gmail.com>
2024-01-11 12:21:47 -0300

    Corrigindo nome do modulo

--- react/aluroni/src/pages/Cardapio/index.tsx
+++ react/aluroni/src/pages/Cardapio/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import styles from "./Cardapio.module.css";
+import styles from "./Cardapio.module.scss";
 import {ReactComponent as Logo} from "assets/logo.svg";
 
 
