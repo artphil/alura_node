3272eca0d9372ae2b9325138a6b90e0e4cff3332
Arthur Phillip Silva <artphil.dev@gmail.com>
2024-01-17 11:51:04 -0300

    filtro e busca funcionando

--- react/aluroni/src/pages/Cardapio/Itens/index.tsx
+++ react/aluroni/src/pages/Cardapio/Itens/index.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from 'react';
 import Item from './item';
-import itensData from './itens.json';
+import cardapio from './itens.json';
 
 import style from './Itens.module.scss';
 
-function Itens() {
+interface Props {
+  busca: string,
+  filtro: number | null,
+  ordenador: string
+}
+
+function Itens(props: Props) {
+  const { busca, filtro } = props;
+  const [lista, setLista] = useState(cardapio);
+
+  function testaBusca(title: string) {
+    const regex =  new RegExp(busca, 'i')
+    return regex.test(title)
+  }
+
+    function testaFiltro(id: number) {
+      if (filtro !== null) return filtro === id;
+      return true;
+    }
+
+  useEffect(() => {
+    const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+    setLista(novaLista);
+  }, [busca,filtro]);
+
   return (
     <div className={style.itens}>
-      {itensData.map(item => (
+      {lista.map(item => (
         <Item {...item} key={item.id} />
       ))}
     </div>
