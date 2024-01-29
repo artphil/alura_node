7795ea7402df480ea458545aa24c0c77eb13e8d0
artphil <artphil.dev@gmail.com>
2024-01-29 11:49:50 -0300

    add back button on motFound page

--- react/aluroni/src/pages/NotFound/index.tsx
+++ react/aluroni/src/pages/NotFound/index.tsx
@@ -4,13 +4,21 @@ import { ReactComponent as NotFoundImage } from 'assets/not_found.svg';
 
 import styles from './NotFound.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
+  const naigate = useNavigate();
+
   return (
     <div className={classNames({
       [styles.container]: true,
       [stylesTema.container]: true
     })}>
+      <div className={styles.voltar}>
+        <button onClick={() => naigate(-1)} >
+          {'< Voltar'}
+        </button>
+      </div>
       <NotFoundImage />
     </div >
   );
